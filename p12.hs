36a6b0108d0ee43dd97eb5383dc2702ac107dde9
Alex Clemmer <clemmer.alexander@gmail.com>
2012-08-09 02:03:28 -0400

    Add working p12

--- p12.hs
+++ p12.hs
@@ -1,40 +1,25 @@
 module P12 where
 
 import List
+import Data.Numbers.Primes (primes)
 
-triangleNums = map (\i -> sum [1..i]) [1..]
-powerset' [] = []
-powerset' (x:xs) = xs : map (x:) (powerset' xs) ++ (powerset' xs)
-powerset s = s : powerset' s
-factors n = [x | x <- [1..(n `div` 2)], n `mod` x == 0] ++ [n]
-trifactors = [factors x | x <- triangleNums]
+-- NOT unique prime factors.
+primeFactorTree 1 = []
+primeFactorTree n = factor : primeFactorTree (n `div` factor)
+             where
+               factor = head $ filter xDividesN primes
+               xDividesN x = (n `mod` x) == 0
 
+-- counts number of ways prime factors can be combined to generate composites
+numCompositeFactors = product . map ((1+) . length) . group . primeFactorTree
 
-range x y = [x..y]
-baserange = [1..500]
-testrange = [1..4]
+-- a triangle number T(n) = sum [1..n]. Since T(n) = T(n - 1) + n, we can
+-- implement this function using dynamic programming:
+triangleNumList = triangleNumList' 1 2
+               where triangleNumList' acc n =
+                       acc : (triangleNumList' (acc + n) (n + 1))
 
-prod (x:[]) = x
-prod (x:xs) = x * prod xs
+triangleNumsAndFactors = [(tri, numCompositeFactors tri) | tri <- triangleNumList]
 
-sumNInts n = (n * (n + 1))/2
-
-
-testval n x = case n < (length $ factors x) of
-  True -> Just True
-  False -> Nothing
-
-test n [] = Nothing
-test n (x:xs) = case testval n x of
-  Nothing -> test n xs
-  Just results -> Just results
-{-
-test n (x:xs) = case (length $ factors x) > n of
-  True -> True
-  False -> test n xs
--}
-
-
-{-
-p12 = find (\x -> snd x >= 500) $ zip triangleNums $ map length trifactors
--}
+main =
+  print $ fst $ head $ filter ((>500) . snd) triangleNumsAndFactors
