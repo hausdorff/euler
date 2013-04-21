caae8895d4624d0c3eb4d3b369c7271154786f1c
Alex Clemmer <clemmer.alexander@gmail.com>
2013-04-21 00:38:44 -0600

    Add more concise solution to p20

--- p20.hs
+++ p20.hs
@@ -1,6 +1,8 @@
 -- uses Haskell's nice arbitrary-sized integer multiplication to simply
 -- generate 100!. We then convert that number to a list of digits and sum
 
+import Char
+
 factorial n = product [1..n]
 
 -- takes integral, produces list of digits
@@ -9,5 +11,8 @@ digits n = (digits (n `div` 10)) ++ [n `mod` 10]
 --[n `mod` k | k <- [10^i | i <- [1..]]]
 
 solution = sum $ digits $ factorial 100
+
+-- a friend's clever solution using nice libraries I didn't know about
+solution' = sum [digitToInt x | x <- (show $ product [1..100])]
 
 main = print solution
