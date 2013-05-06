c743c218e9718948c031a287b3af4c95eaf07f38
Alex Clemmer <clemmer.alexander@gmail.com>
2013-05-05 18:43:27 -0600

    Add p31

---
+++ p31.hs
@@ -0,0 +1,19 @@
+pence :: [Int]
+pence = [1, 2, 5, 10, 20, 50, 100, 200]
+
+total :: [Int] -> Int
+total = sum . zipWith (*) pence
+
+-- if == target, +1
+-- if < target, add a coin
+-- if > target, return
+-- we could start at 0 and add to 200, or we could start at 200 and subtract to
+-- 0. They are equivalent.
+mkChng (x:xs) 0 = 1
+mkChng [] _     = 0
+mkChng (x:xs) total | total > 0 = (mkChng (x:xs) (total-x)) + (mkChng xs total)
+                    | total < 0 = 0
+
+solution = mkChng pence 200
+
+main = print solution
